d77223973b80cf1079a9c62d812b7beb89c8409e
rusla55 <95451314+rusla55@users.noreply.github.com>
2022-05-29 13:33:33 +0300

    Add files via upload

---
+++ 5darbas/server/server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace server
+{
+    public class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+            Sessions.Broadcast(e.Data);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSocketServer server = new WebSocketServer("ws://127.0.0.1:7890");
+            server.AddWebSocketService<Echo>("/Echo");
+            server.Start();
+            Console.WriteLine("Server started");
+            Console.ReadKey();
+            server.Stop();
+        }
+    }
+}
