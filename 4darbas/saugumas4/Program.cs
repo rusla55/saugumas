55a62bea3242bbbd76f9b0b2f3ae3295c84e0d92
rusla55 <95451314+rusla55@users.noreply.github.com>
2022-05-29 13:26:26 +0300

    Add files via upload

---
+++ 4darbas/saugumas4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace saugumas4
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+        }   
+        public static class user
+        {
+            public static int id;
+            public static string key;
+            public static String connection = "datasource=127.0.0.1;port=3306;database=saugumas;username=root;password=";
+        }
+    }
+}
